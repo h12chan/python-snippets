db7d8f424dbb42ddda892dc8ae151f88feb35bea
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-14 16:13:55 -0400

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,30 @@
+# comment declaration
+
+# code blocks determined by indentation
+
+# variable declaration
+mark = 90
+print(mark)
+
+# input
+number = input('What is your mark? ')
+print(number)
+
+# typecasting
+number1, number2 = input('What is your first mark? '), input('What is your second mark?')
+number1 = int(number1)
+number2 = int(number2)
+sum = number1 + number2
+print(sum)
+
+# print f
+number1, number2 = input('What is your first mark? '), input('What is your second mark?')
+number1 = int(number1)
+number2 = int(number2)
+print(f'The first number is {number1}, and the second number is {number2}.')
+
+# BODMAS
+print(6-2+3)
+
+# AND, OR
+TRUE AND FALSE OR TRUE # TRUE
